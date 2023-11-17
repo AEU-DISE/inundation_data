8815f3fa839401b6f6e3bd446a92a50f4d394d7d
lvanceDWR <lisa.vance@water.ca.gov>
2023-11-17 14:26:01 -0800

    troubleshoot error in inundation

--- FW_ISt.R
+++ FW_ISt.R
@@ -16,10 +16,24 @@ ISt <- read_csv("Working files/A02100_Stage_Daily_Mean.csv", skip = 8)
 ISt <- ISt %>%
   rename(DateTime = 'Date Time',
          StageISt = 'Stage Daily Mean (FOOT)',
-         QualityCodeISt = 'Quality Code')
-
+         QualityCodeISt = 'Quality Code') %>%
+  select(-c(QualityCodeISt))
 
 FW <- FW %>%
   rename(DateTime = 'Date Time',
          StageFW = 'Stage Daily Mean (FOOT)',
-         QualityCodeFW = 'Quality Code')
+         QualityCodeFW = 'Quality Code') %>%
+  select(-c(QualityCodeFW))
+
+StagesJoined <- FW %>%
+  left_join(ISt, by = "DateTime")
+
+str(StagesJoined)
+
+Inundation <- StagesJoined %>%
+  mutate(InundationFW = ifelse(StageFW >= 32, "Inundated", "Not Inundated"),
+       InundationISt = ifelse(StageISt >= 17, "Inundated", "Not Inundated")) %>%
+  select(c(DateTime, StageFW, InundationFW, StageISt, InundationISt))
+
+#issue with incorrect inundation values was "" around the numbers. do not put in markdown
+
